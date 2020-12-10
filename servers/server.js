d5980b527439aa5537f64fb7b9dd6530c6a8e7ad
darkdragonP <34474675+darkdragonP@users.noreply.github.com>
2020-12-10 12:34:41 +0900

    first

---
+++ servers/server.js
@@ -0,0 +1,82 @@
+import express from "express";
+import cors from "cors"; //local서버 환경에서 API 연동을 위해서 cors 라이브러리를 설치했습니다.
+import bodyParser from "body-parser"; // 요청을 parser해주는 라이브러리
+import { getRows, getResult } from "./db"; // db.js에서 설명완료
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3001; // 포트 3001
+app.use(bodyParser.json());
+
+app.get("/api/users/:page*?", (req, res) => {
+  // /api/users/:page get으로 요청하면 작동합니다. 다른점은, :page*? 이부분인데, URL의 Params를 받아옵니다. ex) /api/users/1
+  getRows(`SELECT COUNT(*)FROM ADMIN_BOARD`).then((result) => {
+    let page = req.params.page ? req.params.page : 1; // params가 없다면, 1로 자동 세팅하게 해두었습니다.
+    let list = 10; // 보여줄 게시글 수
+    let startRow = (page - 1) * list + 1; // 1페이지일시 1번째 row부터, 2페이지일시 11번재 row부터, 3페이지일시 21번째 row부터
+    let endRow = page * list; // 1페이지일시, 10번째 row까지. 2페이지일시 20번째 row까지. 3페이지일시 30번째 row까지. 페이지가 달라질수록 값이 변합니다.
+    let totalRow = result[0][0]; // 전체 Row의 갯수
+    let totalPage = Math.ceil(totalRow / list); // 30개면 3.
+    // if (endRow > totalPage) endRow = totalPage; // 전체Rows가 26개인데, endRow가 30으로 계산됬을시, 26으로 숫자를 같게 맞춰줍니다
+    getRows(
+      // 위의 변수들을 활용하여, :page 값에 따라 다른 데이터들이 조회됩니다.
+      `
+      SELECT *
+      FROM
+        ( 
+          SELECT /*+ INDEX(b PK1) */
+              u.NAME, b.ADMIN_BOARD_TITLE, 
+              b.ADMIN_BOARD_CONTENTS, b.ADMIN_BOARD_DATE 
+          FROM 
+              ADMIN_BOARD b, USERS u
+          WHERE 
+              ROWNUM <= ${endRow} 
+          AND 
+              b.USER_CODE=u.USER_CODE
+          ORDER BY 
+              b.ADMIN_BOARD_CODE ASC
+        )
+      WHERE
+          ${startRow} <= ROWNUM
+      `
+    ).then((result) => {
+      res.json({ result, totalPage }); // 회원정보들과, 총 페이지의 수를 Return 합니다.
+    });
+  });
+});
+
+app.get("/api/user/:idx", (req, res) => {
+  // idx는 DB에서 고유한 키입니다. 1명의 유저의 정보를 조회할때 사용합니다.
+  const idx = req.params.admin_board_code;
+  getRows(
+    // 조회 쿼리 동일
+    `SELECT USER_CODE, ADMIN_BOARD_TITLE, ADMIN_BOARD_CONTENTS, ADMIN_BOARD_DATE FROM ADMIN_BOARD WHERE ADMIN_BOARD_CODE = ${idx}`
+  ).then((result) => {
+    res.json(result); // 결과 리턴
+  });
+});
+
+app.get("/api/user/detail/:idx", (req, res) => {
+  const idx = req.params.idx;
+  getRows(`SELECT *FROM ADMIN_BOARD WHERE ADMIN_BOARD_CODE = ${idx}`).then(
+    (result) => {
+      res.json(result);
+    }
+  );
+});
+
+app.get("/api/user/search/:id", (req, res) => {
+  // /api/user/search/:id 이부분은, 이름을 URL Params로 받아와 검색해서 일부분일치하는 결과를 알려줍니다.
+  const id = req.params.id;
+  var sql = `SELECT * FROM ADMIN_BOARD WHERE ADMIN_BOARD_TITLE LIKE '%${id}%'`;
+  // ${id}% 는, 예를들어, "박관" 이라고 검색했을때, "박관" 으로 시작하는 모든것을 조회합니다.
+
+  getRows(sql).then((result) => {
+    res.json(result); // 결과리턴
+  });
+});
+
+app.listen(port, () => {
+  // 서버 키는 코드
+  console.log(`express is running on ${port}`);
+});
---
+++ src/pages/Board.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "@rmwc/button"; // React Material 디자인 Button 컴포넌트 입니다.
+import {
+  DataTable,
+  DataTableContent,
+  DataTableHead,
+  DataTableRow,
+  DataTableHeadCell,
+  DataTableBody,
+  DataTableCell,
+} from "@rmwc/data-table"; //React Material 디자인 Data-Table 컴포넌트 입니다.
+import { TextField } from "@rmwc/textfield"; //React Material 디자인 TextField 컴포넌트 입니다.
+import "@rmwc/textfield/styles"; // //React Material TextField 디자인 입니다.
+import "@rmwc/button/styles"; // React Material Button 디자인 CSS 입니다.
+import "@rmwc/data-table/styles"; // React Material Data-Table 디자인 CSS 입니다.
+import "@rmwc/checkbox/styles"; // React Meterial checkbox 디자인 CSS 입니다.
+import "../css/home.css"; // 이 페이지의 커스텀 디자인 CSS 입니다.
+
+const Board = () => {
+  const history = useHistory();
+  const [allChecked, setAllChecked] = useState(false);
+  const [isChecked, setChecked] = useState([]);
+  const [rows, setRows] = React.useState(0);
+  const [cols, setCols] = React.useState(0);
+  const [state, setState] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [globalArray, setGlobalArray] = useState([]);
+  const [tempCount, setTempCount] = useState(0);
+  const [totalPage, setTotalPage] = useState(1);
+
+  useEffect(() => {
+    // 페이지 로딩시 딱 한번만 실행되는 부분입니다. (로딩시 회원정보들을 불러와야 함.)
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3001/api/users/", {
+        //모든 유저들을 불러오는데, api단에서, params가 없으면 기본적으로 1page로 세팅이 되기때문에 처음 1~10개의 데이터만 요청됩니다.
+        method: "GET",
+      });
+      const jsonData = await response.json();
+      setState(jsonData.result); //state 업데이트
+      setTotalPage(jsonData.totalPage);
+    };
+    fetchData();
+  }, [tempCount]); // tempCount가 값이 변하면 리렌더링되는데, 비중이 적어 신경안쓰셔도 될거같습니다.
+
+  const getPageData = async (page) => {
+    // 이부분은 페이지 블록을 클릭했을때 해당 페이지의 정보를 다시 로딩합니다. [2]를 누르면 당연히 2번 페이지가 요청이 되겟죠?
+    const URL = `http://localhost:3001/api/users/${page}`;
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    const jsonData = await response.json();
+    setState(jsonData.result);
+    setTotalPage(jsonData.totalPage); // state 업데이트
+  };
+
+  const searchHandle = (evt) => {
+    // 이부분은 회원정보 검색 input부분 state 업데이트 입니다.
+    setSearchInput(evt.target.value);
+  };
+
+  const searchClick = async (evt) => {
+    // 검색버튼 클릭시 작동합니다.
+    if (searchInput.length < 1) {
+      // 한글자라도 적지않으면 알림창이 나옵니다.
+      alert("검색어를 입력하세요");
+      return;
+      evt.preventDefault();
+    }
+
+    const response = await fetch(
+      `http://localhost:3001/api/user/search/${searchInput}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const jsonData = await response.json();
+    setState(jsonData); // 결과를 state에 반영합니다.
+  };
+
+  const pageClick = (evt) => {
+    // 페이지클릭시 작동
+    const pageNum = evt.currentTarget.id;
+    getPageData(pageNum); // 동일
+  };
+
+  return (
+    <div className="wrapper table_wrapper">
+      <div className="column"></div>
+      <DataTable
+        style={{
+          height: "auto",
+          width: "auto",
+          maxHeight: "1000px",
+          minWidth: "375px",
+          margin: "0px",
+        }}
+        stickyRows={rows}
+        stickyColumns={cols}
+      >
+        <DataTableContent>
+          <DataTableHead>
+            <DataTableRow>
+              <DataTableHeadCell>No</DataTableHeadCell>
+              <DataTableHeadCell>게시자</DataTableHeadCell>
+              <DataTableHeadCell>제목</DataTableHeadCell>
+              <DataTableHeadCell>내용</DataTableHeadCell>
+              <DataTableHeadCell>작성일</DataTableHeadCell>
+            </DataTableRow>
+          </DataTableHead>
+          <DataTableBody>
+            {state.map((v, i) => (
+              <DataTableRow key={i + 1123}>
+                <DataTableCell>{i + 1}</DataTableCell>
+                <DataTableCell>{v[0]}</DataTableCell>
+                <DataTableCell>{v[1]}</DataTableCell>
+                <DataTableCell>
+                  <a href={`/detail/${i + 1}`}>{v[2]}</a>
+                </DataTableCell>
+                <DataTableCell>{v[3]}</DataTableCell>
+              </DataTableRow>
+            ))}
+          </DataTableBody>
+        </DataTableContent>
+      </DataTable>
+      {/* 페이징 처리 */}
+      <center>
+        <div className="column pagination_wrapper">
+          <ul class="pagination">
+            {[...Array(totalPage)].map((n, index) => {
+              return (
+                <li class="page-item">
+                  <b
+                    class="page-link"
+                    className="pagination_number"
+                    value={index + 1}
+                    onClick={pageClick}
+                    id={index + 1}
+                    key={index + 5}
+                  >
+                    [{index + 1}]
+                  </b>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="column">
+          <TextField
+            outlined
+            label="공지제목 검색 란"
+            className="userName searchInput"
+            placeholder="검색할 이름 입력"
+            onChange={searchHandle}
+          />
+          <Button
+            label="공지 제목 조회"
+            outlined
+            className="searchButton homeButtons"
+            onClick={searchClick}
+          />
+        </div>
+      </center>
+    </div>
+  );
+};
+
+export default Board;
